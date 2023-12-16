fcf6a04d1054bd8bb6f277fa66a19d1660b89dc5
JnJuste <jeanjusteirakoze@gmail.com>
2023-12-16 03:20:48 +0200

    .

--- src/main/java/heavencoffee/HeavenCoffeeRestaurant/model/HeavenCoffeeUser.java
+++ src/main/java/heavencoffee/HeavenCoffeeRestaurant/model/HeavenCoffeeUser.java
@@ -9,7 +9,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
